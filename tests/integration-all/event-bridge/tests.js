e19b1e28d0f9871511eaecb7b370b56292fa2543
Ivailo Abadjiev <>
2020-01-03 18:33:29 +0200

    it is suspicious that we did not got timeouts, put few more logs

--- tests/integration-all/event-bridge/tests.js
+++ tests/integration-all/event-bridge/tests.js
@@ -81,6 +81,7 @@ describe('AWS - Event Bridge Integration Test', function() {
       return putEvents('default', putEventEntries)
         .then(() => { console.info('Default Event Bus: waiting for function logs'); return; })
         .then(() => waitForFunctionLogs(tmpDirPath, functionName, markers.start, markers.end))
+        .then(logs => { console.info('Default Event Bus: got function logs'); return logs; })
         .then(logs => {
           expect(logs).to.include(`"source":"${eventSource}"`);
           expect(logs).to.include(`"detail-type":"${putEventEntries[0].DetailType}"`);
@@ -97,6 +98,7 @@ describe('AWS - Event Bridge Integration Test', function() {
       return putEvents(namedEventBusName, putEventEntries)
         .then(() => { console.info('Custom Event Bus: waiting for function logs'); return; })
         .then(() => waitForFunctionLogs(tmpDirPath, functionName, markers.start, markers.end))
+        .then(logs => { console.info('Custom Event Bus: got function logs'); return logs; })
         .then(logs => {
           expect(logs).to.include(`"source":"${eventSource}"`);
           expect(logs).to.include(`"detail-type":"${putEventEntries[0].DetailType}"`);
@@ -113,6 +115,7 @@ describe('AWS - Event Bridge Integration Test', function() {
       return putEvents(arnEventBusName, putEventEntries)
         .then(() => { console.info('Arn Event Bus: waiting for function logs'); return; })
         .then(() => waitForFunctionLogs(tmpDirPath, functionName, markers.start, markers.end))
+        .then(logs => { console.info('Arn Event Bus: got function logs'); return logs; })
         .then(logs => {
           expect(logs).to.include(`"source":"${eventSource}"`);
           expect(logs).to.include(`"detail-type":"${putEventEntries[0].DetailType}"`);
